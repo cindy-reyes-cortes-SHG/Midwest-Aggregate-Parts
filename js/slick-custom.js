9c761ee82c11ea9799e6c1558f3f80c4e7207373
Cindy Reyes-Cortes <cindy-reyes-cortes@sandhills.com>
2026-04-16 16:01:01 -0500

    homepage basically done

--- js/slick-custom.js
+++ js/slick-custom.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
 
-	$('.slider').slick({
+	$('.parts-slider').slick({
   infinite: true,
-  slidesToShow: 2,
+  slidesToShow: 3,
   slidesToScroll: 1,
 	autoplay:true,
-  prevArrow: '<button type="button" class="slick-button slick-prev"><i class="fa-solid fa-arrow-left-long"></i> Previous </button>',
+  prevArrow: false,
   nextArrow: '<button type="button" class="slick-button slick-next">Next <i class="fa-solid fa-arrow-right-long"></i></button>',
 	autoplaySpeed: 5000,
 
@@ -26,5 +26,19 @@ responsive: [
     }
   ]
 });
+
+
+
+	$('.testimonial-slider').slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+	autoplay:true,
+  arrows: false,
+  dots: true,
+	autoplaySpeed: 5000
+});
 	
    });
+
+
